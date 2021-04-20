c17b3e6b284dfdc98a793b989913a133dfa7f09b
amarh-ayman <amarhbishtawi@gmail.com>
2021-04-21 01:45:47 +0300

    finish all except stretch goal

--- js/app.js
+++ js/app.js
@@ -1,64 +1,152 @@
-'use strict';
-let filterArrayOptions = [];
+"use strict";
+/*-----read from json file---- */
+let keyWords = [];
+let dataArray = [];
+let selectedFilter = 0;
 function Image(item) {
   this.title = item.title;
   this.image_url = item.image_url;
   this.description = item.description;
   this.keyword = item.keyword;
   this.horns = item.horns;
+  dataArray.push(this);
 }
 
 Image.prototype.render = function () {
-  let hornClone = $('#photo-template').clone();
-  hornClone.find('h2').text(this.title);
-  hornClone.find('p').text(this.description);
-  hornClone.find('img').attr({
-    src: this.image_url,
-    alt: this.title,
-  });
-  hornClone.attr('class', this.keyword);
-  $('main').append(hornClone);
+  let template = $("#imageTemplate").html();
+  let imageMergedTemplate = Mustache.render(template, this);
+  $("#images_section_main").append(imageMergedTemplate);
 };
 
-/*-----start read from json file---- */
-function renderKeywords() {
+function readJson(number) {
   const ajaxSetting = {
-    method: 'get',
-    datatype: 'json',
+    method: "get",
+    datatype: "json",
   };
-
-  $.ajax('data/page-1.json', ajaxSetting).then(doStuff);
+  if (number === 1) {
+    dataArray = [];
+    $("#images_section_main").children().remove();
+    $.ajax(`data/page-1.json`, ajaxSetting).then(doStuff);
+  }
+  if (number === 2) {
+    dataArray = [];
+    $("#images_section_main").children().remove();
+    $.ajax(`data/page-2.json`, ajaxSetting).then(doStuff);
+  }
 }
-renderKeywords();
+readJson(1);
 
-/*-----end read from json file---- */
 function doStuff(HornData) {
-  HornData.forEach(item => {
+  keyWords = [];
+  sort(HornData);
+  // console.log(dataArray);
+  HornData.forEach((item) => {
     let newImage = new Image(item);
     newImage.render();
 
-    if (!filterArrayOptions.includes(newImage.keyword))
-      filterArrayOptions.push(item.keyword);
+    if (!keyWords.includes(newImage.keyword)) {
+      keyWords.push(newImage.keyword);
+    }
   });
-  filterSelection();
-  $('#photo-template').remove();
-}
 
-$('select').on('change', filter_Function_Event);
+  renderKeywords();
+}
 
 /*--------------filter----------------*/
-function filterSelection() {
-  filterArrayOptions.forEach((item, i) => {
-    let $keyword = $('.option').clone();
-    $keyword.text(item);
-    $('select').append($keyword);
-    $keyword.attr('value', item);
-    $keyword.removeClass('option');
+function renderKeywords() {
+  $("#filter").children().remove();
+  let template = $("#filterTemplate").html();
+  $("#filter").append("<option value='default'>Default</option>");
+  keyWords.forEach((item) => {
+    let x = { y: item };
+    let filterMergedTemplate = Mustache.render(template, x);
+    $("#filter").append(filterMergedTemplate);
   });
 }
 
-function filter_Function_Event() {
+// events functions
+function filterFunction() {
   let select = $(this).val();
-  $('div').hide();
-  $(`.${select}`).show();
+  selectedFilter = select;
+
+  if (select == "default") {
+    $("div").show();
+  } else {
+    $("div").hide();
+    $(`.${select}`).show();
+  }
 }
+
+// events
+$("#filter").on("change", filterFunction);
+
+///////// buttons
+function page1Handler() {
+  readJson(1);
+}
+function page2Handler() {
+  readJson(2);
+}
+
+$("#page1").click(page1Handler);
+$("#page2").on("click", page2Handler);
+
+//////////// sort
+function sortHandler() {
+  let select = $(this).val();
+  if (select === "title") {
+    dataArray.sort((a, b) => {
+      let aTitle = a.title;
+      let bTitle = b.title;
+      if (aTitle < bTitle) {
+        return -1;
+      }
+      if (aTitle > bTitle) {
+        return 1;
+      }
+    });
+  }
+  if (select === "horns") {
+    dataArray.sort((a, b) => {
+      let aHorns = a.horns;
+      let bHorns = b.horns;
+      if (aHorns < bHorns) {
+        return -1;
+      }
+      if (aHorns > bHorns) {
+        return 1;
+      }
+    });
+  }
+  $("#images_section_main").children().remove();
+  dataArray.forEach((element) => {
+    element.render();
+  });
+  if (selectedFilter) {
+    if (selectedFilter === "default") {
+      $("div").show();
+    } else {
+      $("div").hide();
+      console.log(selectedFilter);
+      $(`.${selectedFilter}`).show();
+    }
+  }
+}
+
+$("#sort").on("change", sortHandler);
+function sort(dataArray) {
+  dataArray.sort((a, b) => {
+    let aTitle = a.title;
+    let bTitle = b.title;
+    if (aTitle < bTitle) {
+      return -1;
+    }
+    if (aTitle > bTitle) {
+      return 1;
+    }
+  });
+}
+
+$(".heading").click(function (params) {
+  location.reload();
+});
